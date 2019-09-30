57df94ae30b8ab912f637642bd68d4a6c1b668fc
Sivaranjani Subramanian <sivsubra@Sivaranjanis-MacBook-Pro.local>
2019-09-30 11:04:49 +0100

    Working MVP with three questions for each category

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route("/questions")
-def questions():
-	return "Break the ice!!!"
+
+questions = [
+			 ['Tell me about a time when everything went wrong. How did you deal with it?', 
+			  'Describe a moment when you felt like you truly achieved something',
+			  'What is your favourite childhood memory?'],
+			 ['Are there times when you feel like you do not like your job? If yes, what do you do then?', 
+			  'What is something you wish you knew before you started your career?', 
+			  'Have you ever had conflict with a colleague? How did you deal with it?'],
+			 ['What is your favourite hobby?', 
+			  'Do you have a role model? If yes, who is it? If no, why not?', 
+			  'Is there a book or movie that you think changed you as a person?']
+			]
+
+questions_id = [random.randint(0,2),random.randint(0,2),random.randint(0,2)]
+
+@app.route("/", methods=["POST"])
+def question():
+	category = int(request.form.get("category"))
+	questions_id[category] += 1
+	questions_id[category] %= 3
+	return render_template("index.html", question = questions[category][questions_id[category]])
