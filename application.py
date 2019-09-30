0405ca62de4811cac1e1936e1484d712fd0ccdb4
Sivaranjani Subramanian <siva.subramanian@thoughtworks.com>
2019-09-30 14:32:09 +0100

    Add session variables

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	questions_so_far = []
-	return render_template("index.html")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 questions = [
@@ -21,19 +21,25 @@ questions = [
 			  'Is there a book or movie that you think changed you as a person?']
 			]
 
-questions_id = [random.randint(0,2),random.randint(0,2),random.randint(0,2)]
-questions_so_far = []
-categories_so_far = [0,0,0]
+
+@app.route("/")
+def index():
+	if session.get("questions_so_far") is None:
+		session["questions_so_far"] = []
+		session["questions_id"] = [random.randint(0,2),random.randint(0,2),random.randint(0,2)]
+		session["categories_so_far"] = [0,0,0]
+	return render_template("index.html")
+
 
 @app.route("/", methods=["POST"])
 def question():
 	category = int(request.form.get("category"))
 
-	questions_id[category] += 1
-	questions_id[category] %= 3
-	categories_so_far[category] += 1
+	session["questions_id"][category] += 1
+	session["questions_id"][category] %= 3
+	session["categories_so_far"][category] += 1
 
-	if categories_so_far[category] <= 3:
-		questions_so_far.append(category)
+	if session["categories_so_far"][category] <= 3:
+		session["questions_so_far"].append(category)
 
-	return render_template("index.html", question = questions[category][questions_id[category]], questions_so_far = questions_so_far)
+	return render_template("index.html", question = questions[category][session["questions_id"][category]], questions_so_far = session["questions_so_far"])
